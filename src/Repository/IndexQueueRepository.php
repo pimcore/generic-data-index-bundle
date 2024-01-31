fe4a26e19895e4f271844503d414d6a0e649efc1
Martin Eiber <martin.eiber@pimcore.com>
2024-01-31 07:45:31 +0100

    PD-13 Inject EM into Repository

--- src/Repository/IndexQueueRepository.php
+++ src/Repository/IndexQueueRepository.php
@@ -14,6 +14,8 @@ declare(strict_types=1);
 namespace Pimcore\Bundle\GenericDataIndexBundle\Repository;
 
 use Doctrine\DBAL\Connection;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\EntityRepository;
 use Doctrine\ORM\NonUniqueResultException;
 use Doctrine\ORM\QueryBuilder;
@@ -30,15 +32,12 @@ final class IndexQueueRepository
 {
     use LoggerAwareTrait;
 
-    private readonly ObjectManager $entityManager;
-
     public function __construct(
+        private readonly EntityManagerInterface $entityManager,
         private readonly TimeServiceInterface $timeService,
         private readonly Connection $connection,
         private readonly DenormalizerInterface $denormalizer,
-        ManagerRegistry $managerRegistry,
     ) {
-        $this->entityManager = $managerRegistry->getManager('pimcore_generic_data_index');
     }
 
     public function dispatchableItemExists(): bool
@@ -166,7 +165,6 @@ final class IndexQueueRepository
 
     private function createQueryBuilder(string $alias): QueryBuilder
     {
-        /** @var EntityRepository $repository */
         $repository = $this->entityManager->getRepository(IndexQueue::class);
 
         return $repository->createQueryBuilder($alias);
