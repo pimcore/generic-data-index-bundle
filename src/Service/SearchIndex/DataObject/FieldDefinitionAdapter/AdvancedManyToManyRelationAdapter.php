840eb9d9f2d6bdbe9ec7b0b94802b30e9354b710
Martin Eiber <martin.eiber@pimcore.com>
2024-02-06 11:42:21 +0100

    PD-95 Add AdvancedManyToManyRelationAdapter.

---
+++ src/Service/SearchIndex/DataObject/FieldDefinitionAdapter/AdvancedManyToManyRelationAdapter.php
@@ -0,0 +1,79 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * Pimcore
+ *
+ * This source file is available under following license:
+ * - Pimcore Commercial License (PCL)
+ *
+ * @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ * @license    http://www.pimcore.org/license     PCL
+ */
+
+
+namespace Pimcore\Bundle\GenericDataIndexBundle\Service\SearchIndex\DataObject\FieldDefinitionAdapter;
+
+use InvalidArgumentException;
+use Pimcore\Bundle\GenericDataIndexBundle\Enum\SearchIndex\OpenSearch\AttributeType;
+use Pimcore\Model\DataObject\ClassDefinition\Data\AdvancedManyToManyRelation;
+
+/**
+ * @internal
+ */
+final class AdvancedManyToManyRelationAdapter extends AbstractAdapter
+{
+
+    public function getOpenSearchMapping(): array
+    {
+        $fieldDefinition = $this->getFieldDefinition();
+
+        if (!$fieldDefinition instanceof AdvancedManyToManyRelation) {
+            throw new InvalidArgumentException('FieldDefinition must be of type AdvancedManyToManyRelation');
+        }
+
+        $columnDefinition = $this->getColumnDefinition($fieldDefinition->getColumns());
+
+        return [
+            'properties' => [
+                'fieldname' => [
+                    'type' => AttributeType::KEYWORD,
+                ],
+                'columns' => [
+                    'type' => AttributeType::KEYWORD, // Is actually an array of strings
+                ],
+                'element' => [
+                    'properties' => [
+                        'id' => [
+                            'type' => AttributeType::LONG,
+                        ],
+                        'type' => [
+                            'type' => AttributeType::TEXT,
+                        ],
+                    ],
+                ],
+                'data' => [
+                    'properties' => $columnDefinition
+                ],
+            ],
+        ];
+    }
+
+    private function getColumnDefinition(array $columns): array {
+        $type = [];
+        foreach ($columns as $column) {
+            if (isset($column['type']) && isset($column['key'])) {
+                match ($column['type']) {
+                    'number' => $type[$column['key']] = [
+                        'type' => AttributeType::LONG
+                    ],
+                    default => $type[$column['key']] = [
+                        'type' => AttributeType::KEYWORD
+                    ]
+                };
+            }
+        }
+
+        return $type;
+    }
+}
