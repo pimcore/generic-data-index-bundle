0d0c9cb0b5d814c1552ea04a43e1098136461a71
martineiber <martineiber@users.noreply.github.com>
2024-02-07 11:48:44 +0000

    Apply php-cs-fixer changes

--- src/Service/SearchIndex/DataObject/FieldDefinitionAdapter/ObjectBrickAdapter.php
+++ src/Service/SearchIndex/DataObject/FieldDefinitionAdapter/ObjectBrickAdapter.php
@@ -7,10 +7,9 @@ declare(strict_types=1);
  * This source file is available under following license:
  * - Pimcore Commercial License (PCL)
  *
- * @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
- * @license    http://www.pimcore.org/license     PCL
+ *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ *  @license    http://www.pimcore.org/license     PCL
  */
-
 
 namespace Pimcore\Bundle\GenericDataIndexBundle\Service\SearchIndex\DataObject\FieldDefinitionAdapter;
 
@@ -37,10 +36,9 @@ final class ObjectBrickAdapter extends AbstractAdapter
         }
 
         return [
-            'properties' => $mapping
+            'properties' => $mapping,
         ];
     }
-
 
     private function getMappingForObjectBrick(string $objectBrickType): array
     {
@@ -52,6 +50,7 @@ final class ObjectBrickAdapter extends AbstractAdapter
                 $mapping[$adapter->getOpenSearchAttributeName()] = $adapter->getOpenSearchMapping();
             }
         }
+
         return $mapping;
     }
 }
