0934be1d842a7d331e37d12858ad75b98914f8fa
Christian Fasching <christian.fasching@pimcore.com>
2024-02-13 16:58:22 +0100

    make relations mapping nested

--- src/Service/SearchIndex/DataObject/FieldDefinitionAdapter/AdvancedManyToManyRelationAdapter.php
+++ src/Service/SearchIndex/DataObject/FieldDefinitionAdapter/AdvancedManyToManyRelationAdapter.php
@@ -37,6 +37,7 @@ final class AdvancedManyToManyRelationAdapter extends AbstractAdapter
         $columnDefinition = $this->getColumnDefinition($fieldDefinition->getColumns());
 
         return [
+            'type' => AttributeType::NESTED,
             'properties' => [
                 'fieldname' => [
                     'type' => AttributeType::KEYWORD,
