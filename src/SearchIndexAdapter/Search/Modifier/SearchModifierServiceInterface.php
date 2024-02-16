d45762503377eb8d1bbd7400ce26565eef5dd3bd
markus-moser <markus.moser@pimcore.com>
2024-02-16 12:46:43 +0100

    Remove adapter specific method from interface

--- src/SearchIndexAdapter/Search/Modifier/SearchModifierServiceInterface.php
+++ src/SearchIndexAdapter/Search/Modifier/SearchModifierServiceInterface.php
@@ -13,11 +13,8 @@ declare(strict_types=1);
 
 namespace Pimcore\Bundle\GenericDataIndexBundle\SearchIndexAdapter\Search\Modifier;
 
-use Pimcore\Bundle\GenericDataIndexBundle\Model\OpenSearch\Modifier\SearchModifierContextInterface;
-use Pimcore\Bundle\GenericDataIndexBundle\Model\OpenSearch\Search;
 use Pimcore\Bundle\GenericDataIndexBundle\Model\Search\Interfaces\AdapterSearchInterface;
 use Pimcore\Bundle\GenericDataIndexBundle\Model\Search\Interfaces\SearchInterface;
-use Pimcore\Bundle\GenericDataIndexBundle\Model\Search\Modifier\SearchModifierInterface;
 use Pimcore\Bundle\GenericDataIndexBundle\Service\Search\SearchService\SearchServiceInterface;
 
 interface SearchModifierServiceInterface
@@ -26,11 +23,6 @@ interface SearchModifierServiceInterface
         string $modifierClass,
         object $searchModifierHandlerClass,
         string $method
-    ): void;
-
-    public function applyModifier(
-        SearchModifierInterface $modifier,
-        SearchModifierContextInterface $modifierContext
     ): void;
 
     public function applyModifiersFromSearch(
