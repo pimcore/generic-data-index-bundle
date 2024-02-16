6c452d227c34cc49b9f76169a86b7f38d58297a4
Martin Eiber <martin.eiber@pimcore.com>
2024-02-16 12:35:03 +0100

    PD-95 Add NumericRangeAdapter.

---
+++ tests/Unit/Service/SearchIndex/DataObject/FieldDefinitionAdapter/NumericRangeAdapterTest.php
@@ -0,0 +1,47 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * Pimcore
+ *
+ * This source file is available under following license:
+ * - Pimcore Commercial License (PCL)
+ *
+ * @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ * @license    http://www.pimcore.org/license     PCL
+ */
+
+namespace Pimcore\Bundle\GenericDataIndexBundle\Tests\Unit\Service\SearchIndex\DataObject\FieldDefinitionAdapter;
+
+use Codeception\Test\Unit;
+use Pimcore\Bundle\GenericDataIndexBundle\Service\SearchIndex\DataObject\FieldDefinitionAdapter\NumericRangeAdapter;
+use Pimcore\Bundle\GenericDataIndexBundle\Service\SearchIndex\DataObject\FieldDefinitionServiceInterface;
+use Pimcore\Bundle\GenericDataIndexBundle\Service\SearchIndex\SearchIndexConfigServiceInterface;
+
+/**
+ * @internal
+ */
+final class NumericRangeAdapterTest extends Unit
+{
+    public function testExceptionIsThrownWhenWrongFieldDefinition()
+    {
+        $searchIndexConfigServiceInterfaceMock = $this->makeEmpty(SearchIndexConfigServiceInterface::class);
+        $fieldDefinitionServiceInterfaceMock = $this->makeEmpty(FieldDefinitionServiceInterface::class);
+        $adapter = new NumericRangeAdapter(
+            $searchIndexConfigServiceInterfaceMock,
+            $fieldDefinitionServiceInterfaceMock
+        );
+
+        $mapping = $adapter->getOpenSearchMapping();
+        $this->assertSame([
+            'properties' => [
+                'maximum' => [
+                    'type' => 'float',
+                ],
+                'minimum' => [
+                    'type' => 'float',
+                ],
+            ],
+        ], $mapping);
+    }
+}
