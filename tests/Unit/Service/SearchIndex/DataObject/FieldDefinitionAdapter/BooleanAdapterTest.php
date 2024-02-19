f6df690baf49a84e1f2d3b56a3ef65018a682c06
Martin Eiber <martin.eiber@pimcore.com>
2024-02-19 09:15:34 +0100

    PD-95 Add missing type Adapters pt1 (#53)
    * PD-95 Add BooleanSelect
    
    * PD-95 Add BooleanAdapter Test.
    
    * PD-95 Add QuantityValueAdapter.
    
    * PD-95 Add NumericRangeAdapter.
    
    * Apply php-cs-fixer changes
    
    * PD-95 Add DateRange.
    
    ---------
    
    Co-authored-by: martineiber <martineiber@users.noreply.github.com>

---
+++ tests/Unit/Service/SearchIndex/DataObject/FieldDefinitionAdapter/BooleanAdapterTest.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * Pimcore
+ *
+ * This source file is available under following license:
+ * - Pimcore Commercial License (PCL)
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ *  @license    http://www.pimcore.org/license     PCL
+ */
+
+namespace Pimcore\Bundle\GenericDataIndexBundle\Tests\Unit\Service\SearchIndex\DataObject\FieldDefinitionAdapter;
+
+use Codeception\Test\Unit;
+use Pimcore\Bundle\GenericDataIndexBundle\Service\SearchIndex\DataObject\FieldDefinitionAdapter\BooleanAdapter;
+use Pimcore\Bundle\GenericDataIndexBundle\Service\SearchIndex\DataObject\FieldDefinitionServiceInterface;
+use Pimcore\Bundle\GenericDataIndexBundle\Service\SearchIndex\SearchIndexConfigServiceInterface;
+
+/**
+ * @internal
+ */
+final class BooleanAdapterTest extends Unit
+{
+    public function testGetOpenSearchMapping(): void
+    {
+        $searchIndexConfigServiceInterfaceMock = $this->makeEmpty(SearchIndexConfigServiceInterface::class);
+        $fieldDefinitionServiceInterfaceMock = $this->makeEmpty(FieldDefinitionServiceInterface::class);
+        $adapter = new BooleanAdapter(
+            $searchIndexConfigServiceInterfaceMock,
+            $fieldDefinitionServiceInterfaceMock
+        );
+
+        $mapping = $adapter->getOpenSearchMapping();
+        $this->assertSame([
+            'type' => 'boolean',
+        ], $mapping);
+    }
+}
