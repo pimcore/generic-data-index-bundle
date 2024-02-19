906f52d919fdc8574ab3e83674f7a28ba78a6e91
Martin Eiber <martin.eiber@pimcore.com>
2024-02-19 13:14:16 +0100

    PD-95 Add GeoPoint and GeoBounds.

---
+++ src/Service/SearchIndex/DataObject/FieldDefinitionAdapter/HasLatitudeAnfLongitudeTrait.php
@@ -0,0 +1,37 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * Pimcore
+ *
+ * This source file is available under following license:
+ * - Pimcore Commercial License (PCL)
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ *  @license    http://www.pimcore.org/license     PCL
+ */
+
+
+namespace Pimcore\Bundle\GenericDataIndexBundle\Service\SearchIndex\DataObject\FieldDefinitionAdapter;
+
+use Pimcore\Bundle\GenericDataIndexBundle\Enum\SearchIndex\OpenSearch\AttributeType;
+
+/**
+ * @internal
+ */
+trait HasLatitudeAnfLongitudeTrait
+{
+    private function getLatAndLongMapping(): array
+    {
+        return [
+            'properties' => [
+                'latitude' => [
+                    'type' => AttributeType::FLOAT->value,
+                ],
+                'longitude' => [
+                    'type' => AttributeType::FLOAT->value,
+                ],
+            ],
+        ];
+    }
+}
