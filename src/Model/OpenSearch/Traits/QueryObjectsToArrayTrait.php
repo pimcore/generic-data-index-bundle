594c4ffa40fd9b86e37deead18a4fbdfab76cd9f
markus-moser <markus.moser@pimcore.com>
2024-02-28 12:19:44 +0100

    Improve OpenSearch query models (#83)

---
+++ src/Model/OpenSearch/Traits/QueryObjectsToArrayTrait.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * Pimcore
+ *
+ * This source file is available under following license:
+ * - Pimcore Commercial License (PCL)
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ *  @license    http://www.pimcore.org/license     PCL
+ */
+
+namespace Pimcore\Bundle\GenericDataIndexBundle\Model\OpenSearch\Traits;
+
+use Pimcore\Bundle\GenericDataIndexBundle\Model\OpenSearch\Query\QueryInterface;
+
+trait QueryObjectsToArrayTrait
+{
+    public function convertQueryObjectsToArray(array $params): array
+    {
+        array_walk_recursive(
+            $params,
+            static function (&$value) {
+                if ($value instanceof QueryInterface) {
+                    $value = $value->toArray(true);
+                }
+            }
+        );
+
+        return $params;
+    }
+}
