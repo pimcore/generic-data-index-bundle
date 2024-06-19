457199257f025beeacab917ef447fb7f5cd818de
markus-moser <markus-moser@users.noreply.github.com>
2024-06-19 15:41:13 +0000

    Apply php-cs-fixer changes

--- src/SearchIndexAdapter/OpenSearch/Search/FetchIdsService.php
+++ src/SearchIndexAdapter/OpenSearch/Search/FetchIdsService.php
@@ -1,6 +1,19 @@
 <?php
 declare(strict_types=1);
 
+/**
+ * Pimcore
+ *
+ * This source file is available under two different licenses:
+ * - GNU General Public License version 3 (GPLv3)
+ * - Pimcore Commercial License (PCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ *  @license    http://www.pimcore.org/license     GPLv3 and PCL
+ */
+
 namespace Pimcore\Bundle\GenericDataIndexBundle\SearchIndexAdapter\OpenSearch\Search;
 
 use Pimcore\Bundle\GenericDataIndexBundle\Exception\InvalidArgumentException;
@@ -17,20 +30,19 @@ final readonly class FetchIdsService implements FetchIdsServiceInterface
     public function __construct(
         private FetchIdsBySearchServiceInterface $fetchIdsBySearchService,
         private SearchIndexServiceInterface $searchIndexService,
-    )
-    {
+    ) {
     }
 
     public function fetchIdsForCurrentPage(AdapterSearchInterface $search, string $indexName): array
     {
-        $search = clone($search);
+        $search = clone $search;
         $search->setSource(false);
+
         return $this->searchIndexService
             ->search($search, $indexName)
             ->getIds()
         ;
     }
-
 
     public function fetchAllIds(AdapterSearchInterface $search, string $indexName, bool $sortById = true): array
     {
@@ -40,5 +52,4 @@ final readonly class FetchIdsService implements FetchIdsServiceInterface
 
         return $this->fetchIdsBySearchService->fetchAllIds($search, $indexName, $sortById);
     }
-
 }
--- src/Service/Search/SearchService/TransformToAdapterSearchService.php
+++ src/Service/Search/SearchService/TransformToAdapterSearchService.php
@@ -1,9 +1,21 @@
 <?php
 declare(strict_types=1);
 
+/**
+ * Pimcore
+ *
+ * This source file is available under two different licenses:
+ * - GNU General Public License version 3 (GPLv3)
+ * - Pimcore Commercial License (PCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ *  @license    http://www.pimcore.org/license     GPLv3 and PCL
+ */
+
 namespace Pimcore\Bundle\GenericDataIndexBundle\Service\Search\SearchService;
 
-use Pimcore\Bundle\GenericDataIndexBundle\Exception\InvalidArgumentException;
 use Pimcore\Bundle\GenericDataIndexBundle\Model\Search\Asset\AssetSearch;
 use Pimcore\Bundle\GenericDataIndexBundle\Model\Search\DataObject\DataObjectSearch;
 use Pimcore\Bundle\GenericDataIndexBundle\Model\Search\Document\DocumentSearch;
@@ -22,8 +34,7 @@ final readonly class TransformToAdapterSearchService implements TransformToAdapt
         private DataObject\SearchHelper $dataObjectSearchHelper,
         private Document\SearchHelper $documentSearchHelper,
         private Element\ElementSearchHelperInterface $elementSearchHelper,
-    )
-    {
+    ) {
     }
 
     public function transform(SearchInterface $search, bool $enableOrderByPageNumber = false): AdapterSearchInterface
@@ -41,5 +52,4 @@ final readonly class TransformToAdapterSearchService implements TransformToAdapt
                 => $this->elementSearchHelper->createAdapterSearch($search, $index, $enableOrderByPageNumber),
         };
     }
-
 }
