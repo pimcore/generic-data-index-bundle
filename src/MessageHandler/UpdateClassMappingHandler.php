032b185b495d234237999bead94ee2207104bbab
markus-moser <markus.moser@pimcore.com>
2024-07-24 14:34:43 +0200

    Use reindex when index exists

--- src/MessageHandler/UpdateClassMappingHandler.php
+++ src/MessageHandler/UpdateClassMappingHandler.php
@@ -18,7 +18,9 @@ namespace Pimcore\Bundle\GenericDataIndexBundle\MessageHandler;
 
 use Exception;
 use Pimcore\Bundle\GenericDataIndexBundle\Message\UpdateClassMappingMessage;
+use Pimcore\Bundle\GenericDataIndexBundle\SearchIndexAdapter\SearchIndexServiceInterface;
 use Pimcore\Bundle\GenericDataIndexBundle\Service\SearchIndex\IndexQueue\EnqueueServiceInterface;
+use Pimcore\Bundle\GenericDataIndexBundle\Service\SearchIndex\IndexService\ElementTypeAdapter\DataObjectTypeAdapter;
 use Pimcore\Bundle\GenericDataIndexBundle\Service\SearchIndex\IndexService\IndexHandler\DataObjectIndexHandler;
 use Pimcore\Bundle\GenericDataIndexBundle\Service\SettingsStoreServiceInterface;
 use Pimcore\Bundle\GenericDataIndexBundle\Traits\LoggerAwareTrait;
@@ -34,7 +36,9 @@ final class UpdateClassMappingHandler
 
     public function __construct(
         private readonly DataObjectIndexHandler $dataObjectMappingHandler,
+        private readonly DataObjectTypeAdapter $dataObjectTypeAdapter,
         private readonly EnqueueServiceInterface $enqueueService,
+        private readonly SearchIndexServiceInterface $searchIndexService,
         private readonly SettingsStoreServiceInterface $settingsStoreService,
     ) {
     }
@@ -55,11 +59,20 @@ final class UpdateClassMappingHandler
             return;
         }
 
-        $this->dataObjectMappingHandler
-            ->updateMapping(
-                context: $classDefinition,
-                mappingProperties: $mappingProperties
-            );
+        $alias = $this->dataObjectTypeAdapter->getAliasIndexName($classDefinition);
+        if (!$this->searchIndexService->existsAlias($alias)) {
+            $this->dataObjectMappingHandler
+                ->updateMapping(
+                    context: $classDefinition,
+                    mappingProperties: $mappingProperties
+                );
+        } else {
+            $this->dataObjectMappingHandler
+                ->reindexMapping(
+                    context: $classDefinition,
+                    mappingProperties: $mappingProperties
+                );
+        }
 
         $this->settingsStoreService->storeClassMapping(
             classDefinitionId: $classDefinition->getId(),
