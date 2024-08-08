50eedd2930add024be42dffec99dbf1f2272cd06
markus-moser <markus.moser@pimcore.com>
2024-08-08 15:55:58 +0200

    Create index if not exists while trying to reindex its mapping (#199)

--- src/Service/SearchIndex/IndexService/IndexHandler/AbstractIndexHandler.php
+++ src/Service/SearchIndex/IndexService/IndexHandler/AbstractIndexHandler.php
@@ -65,10 +65,21 @@ abstract class AbstractIndexHandler implements IndexHandlerInterface
         ?ClassDefinition $context = null,
         ?array $mappingProperties = null
     ): void {
-        $this->searchIndexService->reindex(
-            $this->getAliasIndexName($context),
-            $mappingProperties ?: $this->extractMappingProperties($context)
-        );
+        $alias = $this->getAliasIndexName($context);
+        $mappingProperties = $mappingProperties ?: $this->extractMappingProperties($context);
+
+        if (!$this->searchIndexService->existsAlias($alias)) {
+            $this->updateMapping(
+                context: $context,
+                mappingProperties: $mappingProperties
+            );
+        } else {
+            $this->searchIndexService->reindex(
+                $alias,
+                $mappingProperties
+            );
+        }
+
         $this->createGlobalIndexAliases($context);
     }
 
