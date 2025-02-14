f5e8f4b493eb3ea19fb4cb4a2d5082dc92e36f24
Al3x Zamponi <562324+alexz707@users.noreply.github.com>
2025-02-14 14:09:47 +0100

    Update carbon (#285)
    * add nesbot/carbon to composer, adapt to new version
    
    * revert now() adaption
    
    * cs fix
    
    * fix cs
    
    * fix test
    
    * Apply php-cs-fixer changes
    
    * fix test
    
    * Apply php-cs-fixer changes
    
    * fix test
    
    * fix test
    
    ---------
    
    Co-authored-by: alexz707 <alexz707@users.noreply.github.com>

--- tests/Unit/Service/SearchIndex/CachedSearchIndexMappingServiceTest.php
+++ tests/Unit/Service/SearchIndex/CachedSearchIndexMappingServiceTest.php
@@ -19,7 +19,7 @@ namespace Pimcore\Bundle\GenericDataIndexBundle\Tests\Unit\Service\SearchIndex;
 use Codeception\Test\Unit;
 use Pimcore\Bundle\GenericDataIndexBundle\SearchIndexAdapter\SearchIndexServiceInterface;
 use Pimcore\Bundle\GenericDataIndexBundle\Service\SearchIndex\CachedSearchIndexMappingService;
-use Pimcore\Bundle\StaticResolverBundle\Contract\Lib\Cache\RuntimeCacheResolverContract;
+use Pimcore\Bundle\StaticResolverBundle\Lib\Cache\RuntimeCacheResolver;
 
 /**
  * @internal
@@ -36,7 +36,7 @@ final class CachedSearchIndexMappingServiceTest extends Unit
         });
 
         $this->cachedSearchIndexMappingService = new CachedSearchIndexMappingService(
-            new RuntimeCacheResolverContract(),
+            new RuntimeCacheResolver(),
             $searchIndexServiceMock
         );
     }
