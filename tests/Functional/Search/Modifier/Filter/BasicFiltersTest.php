342b16a4927ec6e8f22e5ef63316718b018fc538
lukmzig <lukas.mzigot@pimcore.com>
2025-06-10 09:09:40 +0200

    add boolean value modifier

--- tests/Functional/Search/Modifier/Filter/BasicFiltersTest.php
+++ tests/Functional/Search/Modifier/Filter/BasicFiltersTest.php
@@ -12,11 +12,13 @@
 
 namespace Pimcore\Bundle\GenericDataIndexBundle\Tests\Functional\Search\Modifier\Filter;
 
+use Pimcore\Bundle\GenericDataIndexBundle\Model\Search\Modifier\Filter\Basic\BooleanFilter;
 use Pimcore\Bundle\GenericDataIndexBundle\Model\Search\Modifier\Filter\Basic\ExcludeFoldersFilter;
 use Pimcore\Bundle\GenericDataIndexBundle\Model\Search\Modifier\Filter\Basic\IdFilter;
 use Pimcore\Bundle\GenericDataIndexBundle\Model\Search\Modifier\Filter\Basic\IdsFilter;
 use Pimcore\Bundle\GenericDataIndexBundle\Model\Search\Modifier\Filter\Basic\IntegerFilter;
 use Pimcore\Bundle\GenericDataIndexBundle\Service\Search\SearchService\Asset\AssetSearchServiceInterface;
+use Pimcore\Bundle\GenericDataIndexBundle\Service\Search\SearchService\DataObject\DataObjectSearchServiceInterface;
 use Pimcore\Bundle\GenericDataIndexBundle\Service\Search\SearchService\SearchProviderInterface;
 use Pimcore\Tests\Support\Util\TestHelper;
 
@@ -143,5 +145,31 @@ class BasicFiltersTest extends \Codeception\Test\Unit
         $searchResult = $searchService->search($assetSearch);
         $this->assertCount(1, $searchResult->getItems());
         $this->assertEquals($asset->getId(), $searchResult->getItems()[0]->getId());
+    }
+
+    public function testBooleanFilter()
+    {
+        $objects = TestHelper::createEmptyObjects(count: 3);
+        $objects[0]->setPublished(true)->save();
+
+        /** @var DataObjectSearchServiceInterface $searchService */
+        $searchService = $this->tester->grabService('generic-data-index.test.service.data-object-search-service');
+        /** @var SearchProviderInterface $searchProvider */
+        $searchProvider = $this->tester->grabService(SearchProviderInterface::class);
+
+        $search = $searchProvider
+            ->createDataObjectSearch()
+            ->addModifier(new BooleanFilter('system_fields.published', true))
+        ;
+        $searchResult = $searchService->search($search);
+        $this->assertCount(1, $searchResult->getItems());
+        $this->assertEquals($objects[0]->getId(), $searchResult->getItems()[0]->getId());
+
+        $search = $searchProvider
+            ->createDataObjectSearch()
+            ->addModifier(new BooleanFilter('system_fields.published', false))
+        ;
+        $searchResult = $searchService->search($search);
+        $this->assertCount(2, $searchResult->getItems());
     }
 }
