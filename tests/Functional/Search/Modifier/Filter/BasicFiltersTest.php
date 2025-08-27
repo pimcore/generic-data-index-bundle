a6e905d7b2fa856f77ca371e3978428dd93b73d4
Martin Eiber <martin.eiber@pimcore.com>
2025-08-27 18:30:20 +0200

    Remove Test

--- tests/Functional/Search/Modifier/Filter/BasicFiltersTest.php
+++ tests/Functional/Search/Modifier/Filter/BasicFiltersTest.php
@@ -149,11 +149,9 @@ class BasicFiltersTest extends \Codeception\Test\Unit
 
     public function testBooleanFilter()
     {
-        $objects = TestHelper::createEmptyObjects(count: 4);
+        $objects = TestHelper::createEmptyObjects(count: 3);
         $objects[1]->setPublished(false)->save();
         $objects[2]->setPublished(false)->save();
-
-        $objects[3]->setPublished(null)->save();
 
         /** @var DataObjectSearchServiceInterface $searchService */
         $searchService = $this->tester->grabService(DataObjectSearchServiceInterface::class);
@@ -174,12 +172,5 @@ class BasicFiltersTest extends \Codeception\Test\Unit
         ;
         $searchResult = $searchService->search($search);
         $this->assertCount(2, $searchResult->getItems());
-
-        $search = $searchProvider
-            ->createDataObjectSearch()
-            ->addModifier(new BooleanFilter('system_fields.published', null))
-        ;
-        $searchResult = $searchService->search($search);
-        $this->assertCount(1, $searchResult->getItems());
     }
 }
