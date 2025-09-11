4a86734c7410759f913e1b20766b7a2066a457e1
Marco Perberschlager <marco.perberschlager@pimcore.com>
2025-09-11 13:27:30 +0200

    Revert "Add processing of messages to tests"
    This reverts commit 9dd4285770a2676264fded7805b1d25c74236311.

--- tests/Functional/SearchIndex/AssetBasicTest.php
+++ tests/Functional/SearchIndex/AssetBasicTest.php
@@ -81,9 +81,6 @@ class AssetBasicTest extends \Codeception\Test\Unit
         $folder->setKey('my-test-folder');
         $folder->save();
 
-        //Since the queue is processed asynchronously we need to run the worker here
-        $this->tester->runCommand('messenger:consume', ['--limit'=>2], ['pimcore_generic_data_index_queue']);
-
         $this->assertGreaterThan(
             0,
             Db::get()->fetchOne(
