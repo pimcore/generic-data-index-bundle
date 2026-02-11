1958a6e90604cd21b0c906952580754b62d69f40
Marco Perberschlager <marco.perberschlager@pimcore.com>
2026-02-11 12:59:52 +0100

    Added diagnostics to investigate failing tests

--- tests/Functional/Service/PathServiceTest.php
+++ tests/Functional/Service/PathServiceTest.php
@@ -12,7 +12,9 @@
 
 namespace Pimcore\Bundle\GenericDataIndexBundle\Tests\Functional\Service;
 
+use Pimcore\Bundle\GenericDataIndexBundle\SearchIndexAdapter\PathServiceInterface;
 use Pimcore\Bundle\GenericDataIndexBundle\Service\Search\SearchService\Asset\AssetSearchServiceInterface;
+use Pimcore\Bundle\GenericDataIndexBundle\Service\SearchIndex\SearchIndexConfigServiceInterface;
 use Pimcore\Tests\Support\Util\TestHelper;
 
 class PathServiceTest extends \Codeception\Test\Unit
@@ -47,7 +49,72 @@ class PathServiceTest extends \Codeception\Test\Unit
             ->setKey('test-asset')
             ->save();
 
+        // DIAGNOSTIC: Check maxSynchronousChildrenRenameLimit before rename
+        /** @var SearchIndexConfigServiceInterface $configService */
+        $configService = $this->tester->grabService(SearchIndexConfigServiceInterface::class);
+        $renameLimit = $configService->getMaxSynchronousChildrenRenameLimit();
+        $searchSettings = $configService->getSearchSettings();
+        $this->assertGreaterThan(
+            0,
+            $renameLimit,
+            sprintf(
+                'DIAGNOSTIC: maxSynchronousChildrenRenameLimit is %d (expected 500). searchSettings keys: [%s]. searchSettings dump: %s',
+                $renameLimit,
+                implode(', ', array_keys($searchSettings)),
+                json_encode($searchSettings)
+            )
+        );
+
+        // DIAGNOSTIC: Check folder is indexed before rename
+        /** @var PathServiceInterface $pathService */
+        $pathService = $this->tester->grabService(PathServiceInterface::class);
+        $folderIndexedPath = $pathService->getCurrentIndexFullPath($folder);
+        $this->assertNotNull(
+            $folderIndexedPath,
+            'DIAGNOSTIC: Folder is NOT indexed in OpenSearch before rename'
+        );
+        $this->assertNotEmpty(
+            $folderIndexedPath,
+            'DIAGNOSTIC: Folder indexed path is empty before rename'
+        );
+
+        // DIAGNOSTIC: Check asset is indexed before rename
+        $assetIndexedPathBefore = $pathService->getCurrentIndexFullPath($asset);
+        $this->assertNotNull(
+            $assetIndexedPathBefore,
+            'DIAGNOSTIC: Asset is NOT indexed in OpenSearch before rename'
+        );
+
         $folder->setKey('test-folder')->save();
+
+        // DIAGNOSTIC: Check folder indexed path after rename (should be /test-folder)
+        $folderIndexedPathAfter = $pathService->getCurrentIndexFullPath($folder);
+        $this->assertEquals(
+            '/test-folder',
+            $folderIndexedPathAfter,
+            sprintf(
+                'DIAGNOSTIC: Folder indexed path after rename is "%s" (expected "/test-folder"). Path before rename was "%s"',
+                $folderIndexedPathAfter,
+                $folderIndexedPath
+            )
+        );
+
+        // DIAGNOSTIC: Check asset indexed path after rename (the actual test)
+        $assetIndexedPathAfter = $pathService->getCurrentIndexFullPath($asset);
+        $this->assertEquals(
+            '/test-folder/test-asset',
+            $assetIndexedPathAfter,
+            sprintf(
+                'DIAGNOSTIC: Asset indexed path after rename is "%s" (expected "/test-folder/test-asset"). '
+                . 'Asset path before rename was "%s". '
+                . 'Folder path before rename was "%s". '
+                . 'maxSynchronousChildrenRenameLimit=%d',
+                $assetIndexedPathAfter,
+                $assetIndexedPathBefore,
+                $folderIndexedPath,
+                $renameLimit
+            )
+        );
 
         /** @var AssetSearchServiceInterface $searchService */
         $searchService = $this->tester->grabService('generic-data-index.test.service.asset-search-service');
